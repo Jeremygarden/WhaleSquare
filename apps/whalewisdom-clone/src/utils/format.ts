ebb1a63863da8038ed380cc852a33d7d3f71489a
jeremygarden <gg@github.io>
2026-04-04 12:36:25 +0000

    chore: remove apps/whalewisdom-clone directory

--- apps/whalewisdom-clone/src/utils/format.ts
+++
@@ -1,7 +0,0 @@
-export function formatNumber(value: number) {
-  return value.toLocaleString("en-US");
-}
-
-export function formatPercent(value: number) {
-  return `${(value * 100).toFixed(2)}%`;
-}
