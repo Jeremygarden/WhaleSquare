4b75c30351d008052b3062c9b522a4e74ade52e0
jeremygarden <mingxlee23@gmail.com>
2026-04-04 09:07:29 +0000

    feat: add mock 13F data and zustand store

---
+++ apps/whalewisdom-clone/src/data/mock.ts
@@ -0,0 +1,14 @@
+import type { Institution } from "./types";
+
+export const mockInstitution: Institution = {
+  cik: "0001067983",
+  name: "Sample Capital Management",
+  quarter: "2025 Q4",
+  totalValue: 1245600000,
+  holdings: [
+    { cusip: "037833100", name: "Apple Inc.", ticker: "AAPL", shares: 1200000, value: 225000000, weight: 0.18, changeShares: 200000 },
+    { cusip: "594918104", name: "Microsoft Corp", ticker: "MSFT", shares: 800000, value: 260000000, weight: 0.21, changeShares: -50000 },
+    { cusip: "023135106", name: "Amazon.com Inc", ticker: "AMZN", shares: 600000, value: 198000000, weight: 0.16, changeShares: 100000 },
+    { cusip: "02079K305", name: "Alphabet Inc", ticker: "GOOGL", shares: 450000, value: 150000000, weight: 0.12, changeShares: -80000 },
+  ],
+};
